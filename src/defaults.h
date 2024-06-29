641a33e75f7989175a541e3196ffd04918cd6f70
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-06-29 15:36:59 +0200

    initialize assertion fault

--- src/defaults.h
+++ src/defaults.h
@@ -3,6 +3,7 @@
 #include "canzero/canzero.h"
 
 static void can_defaults() {
+  canzero_set_assertion_fault(error_flag_OK);
   canzero_set_error_arming_failed(error_flag_OK);
   canzero_set_error_heartbeat_miss(error_flag_OK);
   canzero_set_error_precharge_failed(error_flag_OK);
