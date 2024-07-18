e3ddeebac83ba4691a40964926fdd96323781d0f
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-07-18 10:06:04 +0200

    set all errors on default

--- src/defaults.h
+++ src/defaults.h
@@ -7,18 +7,6 @@ static void can_defaults() {
   canzero_set_error_arming_failed(error_flag_OK);
   canzero_set_error_precharge_failed(error_flag_OK);
   canzero_set_error_heartbeat_miss(error_flag_OK);
-  canzero_set_last_node_missed(255);
-
-  canzero_set_error_level_mcu_temperature(error_level_OK);
-  canzero_set_error_level_vdc_voltage(error_level_OK);
-  canzero_set_error_level_input_current(error_level_OK);
-  canzero_set_error_level_magnet_current_left(error_level_OK);
-  canzero_set_error_level_magnet_current_right(error_level_OK);
-  canzero_set_error_level_magnet_temperature_left(error_level_OK);
-  canzero_set_error_level_magnet_temperature_right(error_level_OK);
-
-  canzero_set_ignore_45v(bool_t_FALSE);
-
   canzero_set_error_input_current_invalid(error_flag_OK);
   canzero_set_error_magnet_current_left_invalid(error_flag_OK);
   canzero_set_error_magnet_current_right_invalid(error_flag_OK);
@@ -28,4 +16,15 @@ static void can_defaults() {
   canzero_set_error_mcu_temperature_invalid(error_flag_OK);
   canzero_set_error_magnet_temperature_left_invalid(error_flag_OK);
   canzero_set_error_magnet_temperature_right_invalid(error_flag_OK);
+
+  canzero_set_error_level_mcu_temperature(error_level_OK);
+  canzero_set_error_level_vdc_voltage(error_level_OK);
+  canzero_set_error_level_input_current(error_level_OK);
+  canzero_set_error_level_magnet_current_left(error_level_OK);
+  canzero_set_error_level_magnet_current_right(error_level_OK);
+  canzero_set_error_level_magnet_temperature_left(error_level_OK);
+  canzero_set_error_level_magnet_temperature_right(error_level_OK);
+
+  canzero_set_last_node_missed(255);
+  canzero_set_ignore_45v(bool_t_FALSE);
 }
